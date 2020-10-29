0923cc6ac2a1b0f8c56d8c7dab0ba8401daa1f62
ceress <markmesina69@gmail.com>
2020-10-28 17:50:51 -0700

    init project boiler

---
+++ contracts/TodoList.sol
@@ -0,0 +1,26 @@
+pragma solidity ^0.5.0;
+
+contract TodoList {
+  uint public taskCount = 0; //declare state variable
+
+  struct Task {
+    uint id;
+    string content;
+    bool completed;
+  }
+
+  //create storage for tasks
+  mapping(uint => Task) public tasks;
+
+  //populate todo item
+  constructor() public {
+    createTask ('Check out markmesina on github!');
+  }
+
+  //func to put Task inside storage
+  function createTask(string memory _content) public {
+    taskCount ++; //creates new taskCount
+    tasks[taskCount] = Task(taskCount, _content, false);// reference mapping
+  }
+
+}
