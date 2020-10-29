4f7e439361162c947b9212e1e656a4b3566b5de5
ceress <markmesina69@gmail.com>
2020-10-28 20:44:54 -0700

    init client side toggle func

--- src/app.js
+++ src/app.js
@@ -105,7 +105,7 @@ App = {
         .find('input')
         .prop('name', taskId)
         .prop('checked', taskCompleted)
-        // .on('click', App.toggleCompleted)
+        .on('click', App.toggleCompleted)
 
       //categorize task as completed
       if (taskCompleted) {
@@ -125,7 +125,15 @@ App = {
     window.location.reload()
   },
 
-  
+  toggleCompleted: async (e) => {
+    App.setLoading(true)
+    const taskId = e.target.name
+    await App.todoList.toggleCompleted(taskId)
+    window.location.reload()
+  },
+
+
+
   setLoading: (boolean) => {
     App.loading = boolean
     const loader = $('#loader')
