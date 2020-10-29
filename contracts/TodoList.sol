faa1e02031a971b81c42628cbc294562a3b10e0b
ceress <markmesina69@gmail.com>
2020-10-28 20:40:03 -0700

    init toggleCompleted tests

--- contracts/TodoList.sol
+++ contracts/TodoList.sol
@@ -18,6 +18,11 @@ contract TodoList {
     bool completed
   );
 
+  event TaskCompleted(
+    uint id,
+    bool completed
+  );
+
   //populate todo item
   constructor() public {
     createTask ('Check out markmesina on github!');
@@ -28,8 +33,13 @@ contract TodoList {
     taskCount ++; //creates new taskCount
     tasks[taskCount] = Task(taskCount, _content, false);// reference mapping
     emit TaskCreated(taskCount, _content, false);
-  
-  
   }
 
+  //func to toggle task completion
+  function toggleCompleted(uint _id) public {
+    Task memory _task = tasks[_id];
+    _task.completed = !_task.completed;
+    tasks[_id] = _task;
+    emit TaskCompleted(_id, _task.completed);
+  }
 }
